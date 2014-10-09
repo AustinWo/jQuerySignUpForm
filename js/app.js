9189bf8b0265398728ef599e35bd7f08093bbae7
Walan Worachet <wworachet@Walans-MacBook-Air-2.local>
2014-10-09 00:07:48 -0700

    submit button function

--- js/app.js
+++ js/app.js
@@ -6,9 +6,21 @@ var $confirmPassword = $('#confirm_password');
 // Hide Hints
 $('form span').hide();
 
+function isPasswordValid() {
+  return $password.val().length > 8;
+}
+
+function arePasswordsMatching() {
+  return $password.val() === $($confirmPassword).val();
+}
+
+function canSubmit() {
+  return isPasswordValid() && arePasswordsMatching();
+}
+
 function passwordEvent() {
   // Find out if password is valid
-  if ($password.val().length > 8) {
+  if (isPasswordValid()) {
     // Hide hint if valid
     $password.next().hide();
   } else {
@@ -20,21 +32,25 @@ function passwordEvent() {
 
 function confirmPasswordEvent() {
   //Find out if password and confirmation match
-  if ($password.val() === $($confirmPassword).val()) {
+  if (arePasswordsMatching()) {
       //Hide hint if matched
       $confirmPassword.next().hide();
   } else {
       $confirmPassword.next().show();
   }
+}
 
-
+function enableSubmitEvent() {
+  // !false = true
+  // !true = false
+  $('#submit').prop('disabled', !canSubmit())
 }
 //When event happens on password input
-$password.focus(passwordEvent).keyup(passwordEvent)
-          .focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
+$password.focus(passwordEvent).keyup(passwordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
 // can call 2 focus -> can call multiple handler, no overiding
 
 
 //When event happens on confirmation
-$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
+$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
 
+enableSubmitEvent();
